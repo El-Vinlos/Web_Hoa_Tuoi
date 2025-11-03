cc719b054e0e98fd6d30f0b6eb4d535ba86fe036
El Vinlos <vuongkhanhvinh205@gmail.com>
2025-11-04 05:41:02 +0700

    feat: sort,addtocart

--- WebHoaTuoi/Controllers/SanPhamController.cs
+++ WebHoaTuoi/Controllers/SanPhamController.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
 using WebHoaTuoi.Models;
@@ -12,56 +11,88 @@ namespace WebHoaTuoi.Controllers
     {
         private HoaTuoiDbContext db = new HoaTuoiDbContext();
 
-        // GET: SanPham
-        public ActionResult Index()
+        public ActionResult Index(string loai, string sort)
         {
-            // Lấy danh sách sản phẩm kèm thông tin loại sản phẩm
-            var sanPhams = db.SanPhams.Include("LoaiSP").ToList();
-            return View(sanPhams);
+            // Lấy danh sách sản phẩm kèm loại
+            var sanPhams = db.SanPhams.Include("LoaiSP").AsQueryable();
+
+            // Truyền danh sách loại sang ViewBag
+            ViewBag.DanhMucList = db.LoaiSPs.ToList();
+            ViewBag.SelectedLoai = loai;
+
+            // Lọc theo loại nếu có
+            if (!string.IsNullOrEmpty(loai))
+            {
+                sanPhams = sanPhams.Where(s => s.MaLoaiSP == loai);
+                ViewBag.TenLoai = db.LoaiSPs.Find(loai)?.TenLoaiSP ?? "Danh Sách Sản Phẩm";
+            }
+            else
+            {
+                ViewBag.TenLoai = "Danh Sách Sản Phẩm";
+            }
+
+            // Sắp xếp theo yêu cầu
+            switch (sort)
+            {
+                case "name":
+                    sanPhams = sanPhams.OrderBy(s => s.TenSP);
+                    break;
+                case "price-asc":
+                    sanPhams = sanPhams.OrderBy(s => s.DonGia);
+                    break;
+                case "price-desc":
+                    sanPhams = sanPhams.OrderByDescending(s => s.DonGia);
+                    break;
+            }
+
+            return View(sanPhams.ToList());
         }
 
-        // GET: SanPham/Details/5
         public ActionResult ChiTietSanPham(string id)
         {
             if (id == null)
-            {
                 return HttpNotFound();
-            }
 
-            var sanPham = db.SanPhams.Include("LoaiSP").FirstOrDefault(s => s.MaSP == id);
+            var sanPham = db.SanPhams
+                .Include("LoaiSP")
+                .FirstOrDefault(s => s.MaSP == id);
 
             if (sanPham == null)
-            {
                 return HttpNotFound();
-            }
 
             return View(sanPham);
         }
-
-        // GET: SanPham/TheoLoai/5
-        public ActionResult TheoLoai(string id)
+        
+        [HttpPost]
+        public ActionResult AddToCart(string maSP, int soLuong = 1, string returnUrl = null)
         {
-            if (id == null)
+            var sanPham = db.SanPhams.Find(maSP);
+            if (sanPham != null && sanPham.SoLuong >= soLuong)
             {
-                return RedirectToAction("Index");
+                ShoppingCart.AddToCart(sanPham, soLuong);
+                TempData["CartMessage"] = $"Đã thêm {soLuong} x {sanPham.TenSP} vào giỏ hàng!";
+            }
+            else
+            {
+                TempData["CartMessage"] = "Sản phẩm không tồn tại hoặc hết hàng!";
             }
 
-            var sanPhams = db.SanPhams
-                .Include("LoaiSP")
-                .Where(s => s.MaLoaiSP == id)
-                .ToList();
+            // Redirect back to the page that sent the request
+            if (!string.IsNullOrEmpty(returnUrl))
+                return Redirect(returnUrl);
 
-            ViewBag.TenLoai = db.LoaiSPs.Find(id)?.TenLoaiSP ?? "Tất cả sản phẩm";
+            return RedirectToAction("ChiTietSanPham", new { id = maSP });
+        }
 
-            return View("Index", sanPhams);
+        public ActionResult TheoLoai(string id)
+        {
+            return RedirectToAction("Index", new { loai = id });
         }
 
         protected override void Dispose(bool disposing)
         {
             if (disposing)
-            {
                 db.Dispose();
-            }
             base.Dispose(disposing);
         }
     }
